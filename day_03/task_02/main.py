70c01b48421f7221bc7d949ad97b06d37957bb46
Mitchell Tsutsulis <mitchell444999@gmail.com>
2021-12-12 19:07:35 +1100

    Refactored to remove duplication

--- day_03/task_02/main.py
+++ day_03/task_02/main.py
@@ -1,56 +1,43 @@
 
 if __name__ == '__main__':
-    input_data = open('./input_data')
-    binary_length = len(input_data.readline()) - 1
-    on_binary = []
-    off_binary = []
+    oxygen_generator_rating = 0
+    co2_scrubber_rating = 0
 
-    for bit_position in range(0, binary_length):
-        for data_line in input_data:
-            bit = int(data_line[bit_position])
-
-            if bit == 1:
-                on_binary.append(data_line)
-            elif bit == 0:
-                off_binary.append(data_line)
-
-        if len(on_binary) >= len(off_binary):
-            input_data = on_binary.copy()
-        else:
-            input_data = off_binary.copy()
-
+    # Operation 0 == oxygen_generator_rating
+    # Operation 1 == co2_scrubber_rating
+    for operation_type in range(0, 2):
+        input_data = open('./input_data')
+        binary_length = len(input_data.readline()) - 1
         on_binary = []
         off_binary = []
 
-        if len(input_data) == 1:
-            break
+        for bit_position in range(0, binary_length):
+            for data_line in input_data:
+                bit = int(data_line[bit_position])
 
-    oxygen_generator_rating = int(input_data[0].replace('\n', ''), 2)
+                if bit == 1:
+                    on_binary.append(data_line)
+                elif bit == 0:
+                    off_binary.append(data_line)
 
-    input_data = open('./input_data')
-    on_binary = []
-    off_binary = []
-            
-    for bit_position in range(0, binary_length):
-        for data_line in input_data:
-            bit = int(data_line[bit_position])
+            if_check = len(on_binary) >= len(off_binary)
+            if operation_type == 1:
+                if_check = not if_check
 
-            if bit == 1:
-                on_binary.append(data_line)
-            elif bit == 0:
-                off_binary.append(data_line)
+            if if_check:
+                input_data = on_binary.copy()
+            else:
+                input_data = off_binary.copy()
 
-        if len(on_binary) < len(off_binary):
-            input_data = on_binary.copy()
-        else:
-            input_data = off_binary.copy()
+            on_binary = []
+            off_binary = []
 
-        on_binary = []
-        off_binary = []
+            if len(input_data) == 1:
+                break
 
-        if len(input_data) == 1:
-            break
-
-    co2_scrubber_rating = int(input_data[0].replace('\n', ''), 2)
+        if operation_type == 0:
+            oxygen_generator_rating = int(input_data[0].replace('\n', ''), 2)
+        elif operation_type == 1:
+            co2_scrubber_rating = int(input_data[0].replace('\n', ''), 2)
 
     print(oxygen_generator_rating * co2_scrubber_rating)
